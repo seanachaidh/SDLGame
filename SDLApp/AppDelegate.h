bdb7dad89c47e5092dcc40484b54f301445e4f14
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2020-07-14 09:28:06 +0200

    Initial Commit

---
+++ SDLApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SDLApp
+//
+//  Created by Pieter Van Keymeulen on 14/07/2020.
+//  Copyright © 2020 Pieter Van Keymeulen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ SDLApp/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  SDLApp
+//
+//  Created by Pieter Van Keymeulen on 14/07/2020.
+//  Copyright © 2020 Pieter Van Keymeulen. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
